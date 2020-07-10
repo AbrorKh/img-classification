d3fd42d76220786d718fe92a2256a77fbb9f2dcd
AbrorKh <abrorkhayitbaev@gmail.com>
2020-07-10 14:12:19 -0400

    changes in flow

--- src/UploadImages.js
+++ src/UploadImages.js
@@ -10,6 +10,7 @@ class UploadImages extends React.Component {
           image: null,
           raw_image_link: '',
           predicted: false,
+          show: false,
           predicted_image_link: '',
           raw_image_name: '',
           progress: 0
@@ -42,20 +43,42 @@ class UploadImages extends React.Component {
             console.log(error);
           }, 
           () => {
-            firebase.storage().ref('raw_images').child(image.name).getDownloadURL().then(url => {
+            firebase.storage().ref('raw_images').child(image.name).getDownloadURL().then((url, predicted_url, data) => {
                 console.log("URL FOR RAW: " + url);
                 ref.push({
                     predicted: false,
                     raw_image_link: url,
-                    predicted_image_link: 'PREDICTED_URL_HERE',
+                    predicted_image_link: '',
                     raw_image_name: image.name,
                 })
                 alert('Image Upload was successful');
                 this.setState({url});
+                this.setState({
+                  show: !this.state.show,
+                })
             })
         });
       }
 
+      getClassifiedImage() {
+        firebase.database().ref(`raw_images/`).on('value', 
+        (data) => {
+          // console.log(data.val())
+          var classified = data.val();
+          var keys = Object.keys(classified);
+          // console.log(keys);
+          var k = keys[keys.length-1];
+          var predicted_image = classified[k].predicted_image_link;
+          console.log("Predicted Image link" + predicted_image);
+          this.setState({
+            predicted_image_link: predicted_image
+          });
+        }, 
+        (error) => {
+          console.log(error);
+        }
+        )
+      }
 
       render() {
         return (
@@ -68,17 +91,23 @@ class UploadImages extends React.Component {
                         <div className="display-4 text-secondary">
                         </div><br/>
                             <input type="file" name={"Select"} accept="image/*" onChange={this.handleChange.bind(this)}/><br/><br/>
+                            {/* <br/><br/> */}
+                            <button className="btn btn-outline-primary mr-2" onClick={this.handleUpload.bind(this)}>Submit</button>
+                            { this.state.show ? <button className="btn btn-outline-primary mr-2" onClick={this.getClassifiedImage.bind(this)}>Classify</button> : null}
                             <br/><br/>
                             <ProgressBar animated now={this.state.progress} label={`${this.state.progress}%`} min="0" max="100">
-                            </ProgressBar><br/><br/>
-                            <button className="btn btn-outline-primary mr-2" onClick={this.handleUpload.bind(this)}>Submit</button><br/><br/>
-                            When image is uploaded, it will appear below:
-                            <Image src={this.state.url} thumbnail />
-                            {/* <Image src={this.state.predicted_image_link} thumbnail /> */}
+                            </ProgressBar><br/>             
+                            Images will appear below:            
+                            {/* Uploaded Image will appear below: */}
+                            <Image src={this.state.url} thumbnail/>
+                            {/* Classified Image will appear below: */}
+                            <Image src={this.state.predicted_image_link} thumbnail/>
                         </div>
-
+                        
                     </div>
+                    
                 </div>
+                
         );
     }
 }
